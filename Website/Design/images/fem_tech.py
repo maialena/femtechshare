85ecc6f03a4978766a557761e2164fe41be20450
Maia Rosengarten <maiarosengarten@gmail.com>
2016-07-14 20:17:42 -0700

    adding Website material

---
+++ Website/Design/images/fem_tech.py
@@ -0,0 +1,12 @@
+
+
+def femTech(all_majors_welcome):
+	goal = list('design', 'make', 'share', 'learn')
+	majors = [('compSci': 2), ('artHist': 3), ('bio':4)]
+	for new_member in majors:
+		total += new_member[1];
+		print "Join FemTech"
+	
+
+
+
