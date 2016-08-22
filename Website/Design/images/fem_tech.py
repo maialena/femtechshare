7c46ba43f468fa20a65c662e034fc0d1141cbd9d
Maia Rosengarten <maiarosengarten@gmail.com>
2016-08-21 18:45:06 -0700

    Implemented flexbox, updated labs, changed schedule. Need to finish lab 9/10, make tutorial for lab 5 bootstrap, and fix links on Homework section of Schedule

--- Website/Design/images/fem_tech.py
+++ Website/Design/images/fem_tech.py
@@ -2,10 +2,11 @@
 
 def femTech(all_majors_welcome):
 	goal = list('design', 'make', 'share', 'learn')
-	majors = [('compSci': 2), ('artHist': 3), ('bio':4)]
+	majors = [('compSci', 2), ('artHist', 3), ('bio',4)]
 	for new_member in majors:
-		total += new_member[1];
-		print "Join FemTech"
+		total += new_member[1]	
+		print "Join Our FemTech Community"
+	return total
 	
 
 
---
+++ Website/dom_practice.js
@@ -0,0 +1,5 @@
+/* exercise 1 */
+alert(document.getElementById('header').firstChild.nodeName);
+
+/*exercise 2 */
+alert(document.getElementById('header').lastChild.nodeName);
